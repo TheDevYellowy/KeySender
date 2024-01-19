1852dd44abdafd91939621fb0208e640e9105a31
TheDevYellowy <yellowycorn6963@gmail.com>
2024-01-19 12:16:18 -0600

    replace uiohook-napi keyboard with nutjs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import { WebSocketServer } from "ws";
-import { UiohookKey, uIOhook } from "uiohook-napi";
+import { keyboard } from "@nut-tree/nut-js";
+// import { UiohookKey } from "uiohook-napi";
 
 import { Service, EventLogger } from "node-windows";
 import { dirname } from "path";
@@ -26,6 +27,18 @@ svc.install();
 const server = new WebSocketServer({ port: 1596 });
 var interval = null;
 
+console.log = (text) => {
+  log.info(text);
+};
+
+console.warn = (text) => {
+  log.warn(text);
+};
+
+console.error = (text) => {
+  log.error(text);
+};
+
 function startPingPong(s) {
   s.ping();
   let x = setTimeout(() => {
@@ -50,13 +63,12 @@ server.on("connection", (socket, req) => {
       const str = data.toString();
       const json = JSON.parse(str);
       log.info(`Recieved message event\n${JSON.stringify(json, null, 2)}`);
-      // console.log(`Recieved message event\n${JSON.stringify(json, null, 2)}`);
       switch (json.message) {
         case "keydown":
-          await uIOhook.keyToggle(json.keycode, "down");
+          await keyboard.pressKey(json.keycode);
           break;
         case "keyup":
-          await uIOhook.keyToggle(json.keycode, "up");
+          await keyboard.releaseKey(json.keycode);
           break;
       }
     } catch (e) {}
